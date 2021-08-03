4b4540c4d7020057ea2716789b9b1303e845e42f
alex-1134 <alexa.t.lach@gmail.com>
2021-08-03 17:08:01 +0000

    PPP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,69 @@
-const cardsColor = ['red'. 'red', 'yellow', 'yellow', 'green', 'green', 'greenyellow', 'greenyellow', 'blue', 'blue', 'brown', 'brown', 'purple', 'purple', 'orange', 'orange', 'orangered', 'orangered'];
+document.addEventListener('DOMContentLoaded', () => {
+ 
+    const cardArray = [ 
+        {
+            name: '1',
+            img: 'images/1.jpg'
+        },
+        {
+            name: '2',
+            img: 'images/2.jpg'
+        },
+        {
+            name: '3',
+            img: 'images/3.jpg'
+        },
+        {
+            name: '4',
+            img: 'images/4.jpg'
+        },
+        {
+            name: '5',
+            img: 'images/5.jpg'
+        },
+        {
+            name: '6',
+            img: 'images/6.jpg'
+        },
+        {
+            name: '7',
+            img: 'images/7.jpg'
+        },
+        {
+            name: '8',
+            img: 'images/8.jpg'
+        },
+    ]
+
+    const grid = document.querySelector('.grid')
+
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+           //  card.addEventListener('click', flipcard)
+           grid.appendChild(card)
+        }
+    }
+
+    createBoard()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
